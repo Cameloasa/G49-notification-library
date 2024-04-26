566a900491a26492f533afa074f61d41749df269
Camelia Ciuca <camelia1414@gmail.com>
2024-04-26 11:57:35 +0200

    App

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,38 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import se.lexicon.dao.EmailDao;
+import se.lexicon.dao.EmailDaoImpl;
+import se.lexicon.exception.EmailException;
+import se.lexicon.model.Email;
+import se.lexicon.util.EmailSender;
+
+public class App
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   //Register inform into the database
+        EmailDao emailDao = EmailDaoImpl.getInstance();
+        try {
+
+        //create object
+        Email email = new Email("recipient.exaple@test","Subject test","Content test");
+        //save to the storage
+        Email savedEmail = emailDao.save(email);
+        //Send email
+        EmailSender.sendEmail(savedEmail.getRecipient(),savedEmail.getSubject(),savedEmail.getContent());
+        //Update status
+        savedEmail.setStatus(true);
+        //update in the storage
+        emailDao.update(savedEmail);
+        //find in the storage
+
+        Email foundEmail = emailDao.find(savedEmail.getId());
+        //Print out the object
+        emailDao.findAll().forEach(System.out::println);
+
+        } catch (EmailException e) {
+            System.out.println(e.getMessage());
+
+        }
     }
 }
